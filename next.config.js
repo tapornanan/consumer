05052979f93f759957bed46ef8960d4ff2c9df96
Ta <ta@appsynth.net>
2022-03-07 11:07:19 +0700

    Init 🚀

--- next.config.js
+++ next.config.js
@@ -1,6 +1,51 @@
 /** @type {import('next').NextConfig} */
+
+let merge = require('webpack-merge');
+
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack: (config, { dev, webpack, isServer }) => {
+    const { ModuleFederationPlugin } = webpack.container
+
+    config.experiments = { topLevelAwait: true };
+
+     // we attach next internals to share scope at runtime
+    config.module.rules.push({
+      test: /pages\/_app.[jt]sx?/,
+      loader: "@module-federation/nextjs-mf/lib/federation-loader.js",
+    });
+
+    if (isServer) {
+      Object.assign(config.resolve.alias, {
+        host: false,
+      })
+    } else {
+      config.plugins.push(
+        new ModuleFederationPlugin({
+          remotes: {
+            host: 'host@http://localhost:3000/_next/static/chunks/remoteEntry.js'
+          },
+          shared: {
+            react: {
+              // Notice shared ARE eager here.
+              eager: true,
+              singleton: true,
+              requiredVersion: false,
+            },
+          },
+        })
+      )
+    }
+
+    return merge.merge(config, {
+      entry() {
+        return config.entry().then(entry => {
+          return entry;
+        });
+      },
+    });
+  }
 }
 
 module.exports = nextConfig
